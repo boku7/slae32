b7cf91c852de416922e3a8e3d1c7be3f02d6b3ea
boku <19784872+boku7@users.noreply.github.com>
2019-09-24 15:11:32 -0700

    Create objdump2hex.sh

---
+++ assignment-4/objdump2hex.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Filename: objdump2hex.sh
+# Author:   boku
+OBJFILE=$1
+objdump -d $(pwd)/${1} | grep '[0-9a-f]:' | grep -v 'file'\
+| cut -f2 -d: | cut -f1-6 -d' ' | tr -s ' ' | tr '\t' ' ' \
+| sed 's/ $//g' | sed 's/ /\\x/g' | paste -d '' -s \
+| sed 's/^/"/' | sed 's/$/"/g'
